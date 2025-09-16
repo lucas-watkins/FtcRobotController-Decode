52e1ff48de4fc22943a5dd0a8bf352c34866b7bc
Lucas Watkins <114666925+lucas-watkins@users.noreply.github.com>
2025-09-16 19:12:04 -0400

    Create Example Test

---
+++ TeamCode/src/test/java/ExampleTest.kt
@@ -0,0 +1,12 @@
+import org.junit.Assert
+import org.junit.Test
+
+class ExampleTest {
+
+    @Test
+    fun `print hello world`() {
+        println("Hello World!")
+
+        Assert.assertTrue(true)
+    }
+}
