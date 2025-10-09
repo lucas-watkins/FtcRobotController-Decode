db7273c577f8841c9678da61e29f39a17e97287a
Lucas Watkins <114666925+lucas-watkins@users.noreply.github.com>
2025-10-08 22:17:09 -0400

    Unit Conversion Helper Class

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/modular/UnitsTest.kt
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.modular
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+import org.firstinspires.ftc.teamcode.modular.Units
+
+class UnitsTest {
+    private fun isInEpsilon(x: Double, y: Double, epsilon: Double = 0.000001): Boolean {
+        return x in (y - epsilon)..(y + epsilon)
+    }
+
+    @Test
+    fun `inches to feet`() {
+        assertTrue(isInEpsilon(144.0, Units.convert(12, Units.FT, Units.IN)))
+    }
+
+    @Test
+    fun `inches to tile`() {
+        assertTrue(isInEpsilon(1.0, Units.convert(24, Units.IN, Units.TILE)))
+    }
+
+    @Test
+    fun `tile to feet`() {
+        assertTrue(isInEpsilon(2.0, Units.convert(1.0, Units.TILE, Units.FT)))
+    }
+
+    // Freshmen labor starts here...
+
+}
