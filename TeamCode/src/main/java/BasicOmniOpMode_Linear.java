58d317e18022b7d543f5399b3297520b3ca416f3
AGOODFROG <caden.j.quirion@gmail.com>
2025-10-18 00:48:15 -0400

    removed and depercated files that are to be rewrote
     the BasicOmmiOpMode_Linear will be moved to a iterative op mode in
     kotlin. It is being kept for documentation

--- TeamCode/src/main/java/BasicOmniOpMode_Linear.java
+++ TeamCode/src/main/java/BasicOmniOpMode_Linear.java
@@ -58,7 +58,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
+   // this is deprecated, and is being kept as it is a good source of documentation
+
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
+@Disabled
 
 public class BasicOmniOpMode_Linear extends LinearOpMode {
 
