adbe85bbda34d038a401a816914af965795fb1dc
Lucas Watkins <114666925+lucas-watkins@users.noreply.github.com>
2025-11-01 13:22:10 -0400

    Fix rounding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modular/Units.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modular/Units.kt
@@ -5,8 +5,8 @@ enum class Units(private val convFactors: Array<Double>) {
     TILE(arrayOf(1.0, 24.0, 2.0, 60.69, 0.6069)),
     IN(arrayOf(0.0416666667, 1.0, 0.0833333333, 2.54, 0.0254)),
     FT(arrayOf(0.5, 12.0, 1.0, 30.48, 0.3048)),
-    CM(arrayOf(0.0164771791, 0.3937007874, 0.032808399, 1.0, 100.0)),
-    M(arrayOf(1.64771791, 39.37007874, 3.2808399, 100.0, 1.0));
+    CM(arrayOf(0.01640419948, 0.3937007874, 0.032808399, 1.0, 100.0)),
+    M(arrayOf(1.640419948, 39.37007874, 3.2808399, 100.0, 1.0));
 
     fun to(n: Number, destUnit: Units): Double {
         return convert(n, this, destUnit)
