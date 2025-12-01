defd2870b582ece8cde3fb00b6ddd81efa00f796
AGOODFROG <caden.j.quirion@gmail.com>
2025-11-30 19:53:39 -0500

    Reapply "moved war teli-op to old teli-op for merging perpouse"
    This reverts commit 1e8d0baf67790296049afc30e0ab6d677fcbe818.
    
    opps

--- TeamCode/src/main/java/workingTeliOp.kt
+++ TeamCode/src/main/java/workingTeliOp.kt
@@ -8,6 +8,21 @@ import org.firstinspires.ftc.teamcode.modular.BaseOpMode
 import kotlin.math.abs
 
 
+/*
+* This file contains an example of an iterative (Non-Linear) "OpMode".
+* An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+* The names of OpModes appear on the menu of the FTC Driver Station.
+* When a selection is made from the menu, the corresponding OpMode
+* class is instantiated on the Robot Controller and executed.
+*
+* This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+* It includes all the skeletal structure that all iterative OpModes contain.
+*
+* Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+* Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+*/
+
+
 @TeleOp(name = "war teli-op", group = "Iterative OpMode")
 //@Disabled
 class workingTeliOp : BaseOpMode() {
@@ -150,7 +165,7 @@ class workingTeliOp : BaseOpMode() {
             launchSpeed = 0.0
         }
         //telemetry.addData("max speed ", maxLaunchSpeed)
-       // telemetry.addData("speed ", launchSpeed)
+        // telemetry.addData("speed ", launchSpeed)
 
 
 
@@ -174,7 +189,7 @@ class workingTeliOp : BaseOpMode() {
         leftLauncherMotor.setVelocity(launchSpeed, AngleUnit.RADIANS)
         rightLauncherMotor.setVelocity(launchSpeed, AngleUnit.RADIANS)
 
-       // avgVelocity = ((leftLauncherMotor.getVelocity(AngleUnit.RADIANS) + rightLauncherMotor.getVelocity(AngleUnit.RADIANS)) / 2)
+        // avgVelocity = ((leftLauncherMotor.getVelocity(AngleUnit.RADIANS) + rightLauncherMotor.getVelocity(AngleUnit.RADIANS)) / 2)
 
         telemetry.addData("launchSpeedSet: ", launchSpeed)
         telemetry.addData("left launch speed radian: ", leftLauncherMotor.getVelocity(AngleUnit.RADIANS))
@@ -195,18 +210,3 @@ class workingTeliOp : BaseOpMode() {
     override fun stop() {
     }
 }
-
-
-/*
-* This file contains an example of an iterative (Non-Linear) "OpMode".
-* An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
-* The names of OpModes appear on the menu of the FTC Driver Station.
-* When a selection is made from the menu, the corresponding OpMode
-* class is instantiated on the Robot Controller and executed.
-*
-* This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
-* It includes all the skeletal structure that all iterative OpModes contain.
-*
-* Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
-* Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
-*/
