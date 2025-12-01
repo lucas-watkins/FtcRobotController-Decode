5c03362a39be5222d33cdc5d516f42fd86e35e6a
AGOODFROG <caden.j.quirion@gmail.com>
2025-11-30 19:27:06 -0500

    started removing stuff and other fun things
    switch some constant vars to vals chageing types so it works with ticks so on and so forth

--- TeamCode/src/main/java/TeliOpMode_Iterative.kt
+++ TeamCode/src/main/java/TeliOpMode_Iterative.kt
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.util.ElapsedTime
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.teamcode.modular.BaseOpMode
 import kotlin.math.abs
 
@@ -25,28 +24,24 @@ import kotlin.math.abs
 @TeleOp(name = "Teliop kotlin", group = "Iterative OpMode")
 //@Disabled
 class TeliOpMode_Iterative : BaseOpMode() {
-    // Declare OpMode members.
-
-    // Declare OpMode members.
-
 
     var runtime : ElapsedTime = ElapsedTime()
-    private var axialMotion = 0.0 // Note: pushing stick forward gives negative value
-    private var lateralMotion = 0.0 // side to
-    private var yawMotion = 0.0
-    private var launchSpeed: Double = 0.0
+    private var forwardMotion = 0.0 // -gamepad1.left_stick_y, as pushing stick forward gives negative value
+    private var lateralMotion = 0.0 // gamepad1.left_stick_x
+    private var yawMotion = 0.0 // gamepad1.right_stick_x
+    private var launchSpeed: Int = 0
 
-    private var powerSetting = 0.25
+    private val powerMultiplier = arrayOf(0.25, 0.5, 0.66)// todo add cycle controls
 
     // in this could be faster but their is no reason to make it faster
     // in the configuration of the robot as of nov 4 2700 is if anything too powerful
     // the absolute maxPowers 2770 this could cause issues with power getting to the motor
 
-    private var maxLaunchSpeed: Double = 2500.0
+    private val maxLaunchSpeed = 2500
 
-    private var avgVelocity = 0.0 // radians
+    private var avgVelocity = 0// ticks
 
-    private var maxDriveMotorPower: Double = 2500.0
+    private val maxDriveMotorPower = 2500
     /*
      * Code to run ONCE when the driver hits INIT
      */
@@ -69,35 +64,23 @@ class TeliOpMode_Iterative : BaseOpMode() {
      * Code to run REPEATEDLY after the driver hits START but before they hit STOP
      */
     override fun loop() {
-        axialMotion = -gamepad1.left_stick_y.toDouble() // Note: pushing stick forward gives negative value
+        forwardMotion = -gamepad1.left_stick_y.toDouble() // Note: pushing stick forward gives negative value
         lateralMotion = gamepad1.left_stick_x.toDouble()
         yawMotion = gamepad1.right_stick_x.toDouble()
 
-        /*
-        if(launchSpeed == 0){
-            leftLauncherMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            rightLauncherMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        }
-        */
 
         // Combine the joystick requests for each axis-motion to determine each wheel's power.
         // Set up a variable for each drive wheel to save the power level for telemetry.
 
 
         val motorPowers = arrayOf(
-            axialMotion + lateralMotion + yawMotion,
-            axialMotion - lateralMotion - yawMotion,
-            axialMotion - lateralMotion + yawMotion,
-            axialMotion + lateralMotion - yawMotion,
+            forwardMotion + lateralMotion + yawMotion,
+            forwardMotion - lateralMotion - yawMotion,
+            forwardMotion - lateralMotion + yawMotion,
+            forwardMotion + lateralMotion - yawMotion,
         )
 
-        if( gamepad1.dpad_up){
-            powerSetting = 0.25
-        }else if(gamepad1.dpad_left){
-            powerSetting = 0.5
-        }else if(gamepad1.dpad_down){
-            powerSetting = 0.66
-        }
+
 
 
         // Normalize the values so no wheel power exceeds 100%
@@ -110,7 +93,7 @@ class TeliOpMode_Iterative : BaseOpMode() {
         // power setting will come after
 
         // the motors should not be normalised to unless a index of motorPower is grater then one
-        driveTrain.forEachIndexed {i, m -> m.power = motorPowers[i] * powerSetting}
+        driveTrain.forEachIndexed {i, m -> m.power = motorPowers[i] * powerMultiplier}
 
         if(maxDriveMotorPower > 1){
             motorPowers.forEachIndexed {i, m -> motorPowers[i] /= abs(maxDriveMotorPower)}
@@ -126,13 +109,13 @@ class TeliOpMode_Iterative : BaseOpMode() {
          */
 
         if (gamepad2.aWasPressed()) {
-            launchSpeed += 50
+            launchSpeed += 50 // ticks
         } else if (gamepad2.bWasPressed()) {
-            launchSpeed -= 50
+            launchSpeed -= 50 //ticks
         }
 
         if(launchSpeed > maxLaunchSpeed){
-            launchSpeed = maxLaunchSpeed.toDouble()
+            launchSpeed = maxLaunchSpeed
         }
         telemetry.addData("max speed ", maxLaunchSpeed)
         telemetry.addData("speed ", launchSpeed)
@@ -169,7 +152,7 @@ class TeliOpMode_Iterative : BaseOpMode() {
         telemetry.addData("left launch speed: ", rightLauncherMotor.velocity)
         telemetry.addData("avg speed: ", avgVelocity)
 
-        telemetry. addData("power setting: ", powerSetting)
+        telemetry. addData("power setting: ", powerMultiplier)
 
         telemetry.update()
     }
