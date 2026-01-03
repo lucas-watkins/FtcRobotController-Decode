5a77e69d61c08c772bcfb3380653d3af04d30115
wgrant-tgs <147338880+wgrant-tgs@users.noreply.github.com>
2026-01-03 12:07:42 -0500

    removed unused imports in the apriltag detection test class

--- TeamCode/src/main/java/AprilTagDetectionTest.kt
+++ TeamCode/src/main/java/AprilTagDetectionTest.kt
@@ -1,7 +1,5 @@
-import com.qualcomm.hardware.limelightvision.Limelight3A
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import kotlin.jvm.javaClass
 
 @TeleOp
 class AprilTagDetectionTest : LinearOpMode() {
