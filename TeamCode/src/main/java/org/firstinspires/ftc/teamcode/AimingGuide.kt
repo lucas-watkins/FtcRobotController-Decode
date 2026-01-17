ff8f657b57548502f5bec87597dffa42f5b92bf1
wgrant-tgs <147338880+wgrant-tgs@users.noreply.github.com>
2026-01-17 15:16:43 -0500

    500ms block

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AimingGuide.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AimingGuide.kt
@@ -2,30 +2,40 @@ package org.firstinspires.ftc.teamcode
 
 import org.firstinspires.ftc.teamcode.modular.GoBildaPrismDriver.GoBildaPrismDriver
 import org.firstinspires.ftc.teamcode.modular.LedStrip
+import kotlin.time.DurationUnit
+import kotlin.time.TimeMark
+import kotlin.time.TimeSource
 
 class AimingGuide {
     var strip: LedStrip
     var localization: Localization
+    private val timeSource = TimeSource.Monotonic
+    private var lastMark: TimeMark
 
-    fun update() : Unit {
+    fun update() {
         val angle = localization.angleToGoal
-        if (angle == null) {
-            strip.setColorRed()
-            return
+
+        if (lastMark.elapsedNow().toLong(DurationUnit.MILLISECONDS) > 500) {
+            if (angle == null) {
+                strip.setColorRed()
+                return
+            }
+
+            if (angle >= -5 && angle <= 5) {
+                strip.setColorGreen()
+                return
+            }
+
+            if (angle < 5) {
+                strip.setColorHalfGreenHalfRed()
+            }
+
+            if (angle > -5) {
+                strip.setColorHalfRedHalfGreen()
+            }
         }
 
-        if (angle >= -5 && angle <= 5) {
-            strip.setColorGreen()
-            return
-        }
-
-        if (angle < 5) {
-            strip.setColorHalfGreenHalfRed()
-        }
-
-        if (angle > -5) {
-            strip.setColorHalfRedHalfGreen()
-        }
+        lastMark = timeSource.markNow()
     }
 
     override fun toString() : String {
@@ -41,5 +51,6 @@ class AimingGuide {
     constructor(driver: GoBildaPrismDriver, localization: Localization) {
         this.localization = localization
         strip = LedStrip(driver)
+        lastMark = timeSource.markNow()
     }
 }
